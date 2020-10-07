9daa393ba3e27b9d80162f0c6a17a03046c92e36
faithlierheimer <flierheimer@gmail.com>
2020-10-07 14:40:18 -0600

    initial setup!

---
+++ static/js/logic.js
@@ -0,0 +1,13 @@
+//initial map setup 
+var myMap = L.map("map").setView([37.0902, -95.7129], 5);
+
+//set initial tile layer using api key
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
